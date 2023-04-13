81b2369450f8b50ca238ba3a66f52a63e4e29777
rstriquer <rstriquer@gmail.com>
2023-04-13 02:54:45 -0300

    docs: Add base docblock to swagger header on the project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenAPi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *   title="Pet Shop Server", version="1.0-rc",
+ *   description="<h2>Limits</h2> We will send 429 (too many requests) HTTP CODE whenever necessary."
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
